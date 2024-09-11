bbbf72406dff9f505d18132381b842debd06c7de
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-09-11 04:26:05 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    result;
+    constructor() {
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result =  (Math.ceil(this.min) + Math.ceil(this.max))/2;
+        return Math.ceil(this.result);
     }
 
     lower() {
-
+        this.max = this.result;
+        return this.max
     }
 
     greater() {
-
+        this.min = this.result;
+        return this.min
     }
 }
 
